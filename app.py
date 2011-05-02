f0877d3949d044d866978ea3121116dd422a0a70
Dane Springmeyer <dane@dbsgeo.com>
2011-05-01 23:22:32 -0700

    start returning raster summary, next step actually grab wkt and use it to mask the raster

--- app.py
+++ app.py
@@ -40,29 +40,33 @@ def application(environ, start_response):
         # parse the query string
         if query:
             params = parse_qs(query)
-        if False:# (query):
-            response = 'query needed which contains wkt=something'
-        else:
-            # bring in the raster using agoodle:
-            g = AGoodle('demo_data/clay_sub1_wgs84.tif')
-            print "raster extent acquired"
-            bbox = (-78.6920697, -78.6842881, 38.1767129, 38.1863279) #specifies extent
-            print "bounding box established"
-            arr = g.read_array_bbox()
-            print "array established"
-            # to feed in user-drawn polgon: wkt = """POLYGON ((-78.6920697 38.1767129, -78.6920697 38.1863279, -78.6842881 38.1863279, -78.6842881 38.1767129, -78.6920697 38.1767129))"""
-            # print "polygon imported to wkt"
-            # arr.mask_with_poly(wkt)
-            # print "geometry acquired"
-            ranges = [(0, 45), (45, 50), (50, 60), (60, 75)] # establishes data output binned classes
-            cell_area = abs(g.raster_info.xsize * g.raster_info.ysize)
-            for vmin, vmax in ranges:
-            	subset = arr[(arr > vmin) & (arr <= vmax)]
-            	print vmin, vmax, len(subset), len(subset) * cell_area
-            print "agoodle-ized"
-            mime = 'text/plain'
-            data = {'vmin':'vmax'}
-            response = json.dumps(data)
+            if params.get('wgs84_wkt'):
+                # bring in the raster using agoodle:
+                g = AGoodle('demo_data/clay_sub1_wgs84.tif')
+                bbox = (-78.6920697, -78.6842881, 38.1767129, 38.1863279) #specifies extent
+                arr = g.read_array_bbox(bbox)
+                # to feed in user-drawn polgon: wkt = """POLYGON ((-78.6920697 38.1767129, -78.6920697 38.1863279, -78.6842881 38.1863279, -78.6842881 38.1767129, -78.6920697 38.1767129))"""
+                # arr.mask_with_poly(wkt)
+                cell_area = abs(g.raster_info.xsize * g.raster_info.ysize)
+                response = {}
+                response['sum'] = str(arr.sum())
+                response['max'] = str(arr.max())
+                response['min'] = str(arr.min())
+                response['mean'] = str(arr.mean())
+                response['data_type'] = str(arr.dtype)
+                response['area'] = str(len(arr) * cell_area)
+                #import pdb;pdb.set_trace()
+                
+                # bin continuous into ranges
+                #ranges = [(0, 45), (45, 50), (50, 60), (60, 75)] # establishes data output binned classes
+                #for vmin, vmax in ranges:
+                	#subset = arr[(arr > vmin) & (arr <= vmax)]
+                	#print vmin, vmax, len(subset), len(subset) * cell_area
+                
+                mime = 'text/plain'
+                response = json.dumps(response)
+            else:
+                response = '{"error":"wgs84_wkt param was empty!"}'                
         else:
             response = '{"error":"nematodes without data are not sexy!"}'
     # root of app requested
@@ -71,8 +75,6 @@ def application(environ, start_response):
         response = FileWrapper(open('index.html','rb'))
     elif (path_info == '/favicon.ico'):
         response = 'favicons are silly'
-    elif (path_info == '/query/'):
-        
     else:
         # assume it is a file name
         filename = os.path.basename(path_info)
